01abd661614712396209362d81ffab584b446040
Vic <93591139+v2ctor@users.noreply.github.com>
2022-11-30 13:05:08 +0300

    Fix routes

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return 'Rekruto! Давай дружить!'
-
-
-@app.get('/url_name')
-def hello():
     name = request.args.get('name')
     message = request.args.get('message')
-    return 'Hello {}! {}!'.format(name, message)
+    if name and message:
+        return 'Hello {}! {}!'.format(name, message)
+    else:
+        return 'Rekruto! Давай дружить!'
